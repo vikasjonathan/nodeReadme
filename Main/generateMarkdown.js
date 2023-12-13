9b2932f07e7653d655d3e24b3086d3869fef7ed5
Vikas David Jonathan <vikas.jonathan@gmail.com>
2023-12-13 11:41:29 -0600

    Added generate file for markdonw and badges

---
+++ Main/generateMarkdown.js
@@ -0,0 +1,73 @@
+
+function renderLicenseBadge(data) {
+
+    if (data.license == 'Apache 2.0') {
+      badge = `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
+    } else if (data.license == 'MIT'){
+      badge = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+    } else if (data.license == 'GPLv2') {
+      badge = `![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)`
+    } else if (data.license == 'GPLv3') {
+      badge = `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+    } else if (data.license == 'BSD 3-clause') {
+      badge = `![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)`
+    } else {
+      badge = ''
+    }
+  
+  }
+  
+  // TODO: Create a function that returns the license link
+  
+  function renderLicenseLink(data) {
+    if (data.license == 'Apache 2.0') {
+      licenseLink = `https://opensource.org/licenses/Apache-2.0`
+    } else if (data.license == 'MIT'){
+      licenseLink = `https://opensource.org/licenses/MIT`
+    } else if (data.license == 'GPLv2') {
+      licenseLink = `https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html`
+    } else if (data.license == 'GPLv3') {
+      licenseLink = `https://www.gnu.org/licenses/gpl-3.0`
+    } else if (data.license == 'BSD 3-clause') {
+      licenseLink = `https://opensource.org/licenses/BSD-3-Clause`
+    } else {
+      licenseLink = ''
+  }
+  }
+  
+  // TODO: Create a function that returns the license section of README
+ 
+  function renderLicenseSection(data) {
+    if (data.license == 'none') {
+      licenseSection = 'N/A'
+    } else {
+      licenseSection = `This application is covered under a(n) <a href = "${licenseLink}">${data.license}</a> license`
+    }
+  
+  }
+  
+  // TODO: Create a function to generate markdown for README
+  function generateMarkdown(data){
+    renderLicenseLink(data)
+    renderLicenseBadge(data)
+    renderLicenseSection(data)
+    return `# ${data.title} <a href= "${licenseLink}">${badge}</a>
+
+  ## Description
+    ${data.description}
+
+  ## Table of content
+  [Installation](#installation)
+  [Usage](#usage)
+  [License](#license)
+  [Contributing](#contributing)
+  [Tests](#tests)
+  [Questions](#questions)
+
+    
+    
+    
+    `;
+  }
+
+module.exports= generateMarkdown;
--- Main/index.js
+++ Main/index.js
@@ -1,102 +1,74 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-let licenceInfromation='';
-inquirer
-    .prompt([
-        {
-            name: 'title',
-            type: 'input',
-            message: 'What is your project title',
-        },
-        {
-            name: 'discription',
-            type: 'input',
-            message: 'Enter discription of your project',
-        },
-        {
-            name: 'instructions',
-            type: 'input',
-            message: 'Enter instructions for your project',
-        },
-        {
-            name: 'usageInformation',
-            type: 'input',
-            message: 'Enter Usage Information  of your project',
-        },
-        {
-            name: 'contributionGuidelines',
-            type: 'input',
-            message: 'Enter Contributions of your project',
-        },
-        {
-            name: 'testInstruction',
-            type: 'input',
-            message: 'Enter Test instructions of your project',
-        },
-        {
-            name: 'githubName',
-            type: 'input',
-            message: 'Enter your Git user name',
-        },
-        {
-            type: 'checkbox',
-            name: 'licences',
-            message:'Choose a license',
-            choices:['Academic Free License v3.0',
-            new inquirer.Separator(),
-            'Apache license 2.0',
-            new inquirer.Separator(),
-            'MIT',
-            new inquirer.Separator(),
-            'The Unlicense'],
-        },
+//packages needed for application
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateMarkdown = require("./generateMarkdown.js");
+
+// array of questions for inquirer
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a brief description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter instructions for installation.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter the usage information for your project.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select the license for your project.',
+        choices: ['MIT', 'GPLv2', 'Apache 2.0', 'GPLv3', 'BSD 3-clause', 'none'],
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please enter any contribution guidelines for your project.',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please enter any test instructions for your project.',
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    }
+];
 
 
-    ])
-    .then((answers) => {
-        console.log(answers.licences)
-      /* if(answers.licences=="Academic Free License v3.0")
-            licenceInfromation="This Academic Free License (the “License”) applies to any original work of authorship (the “Original Work”) whose owner (the “Licensor”) has placed the following licensing notice adjacent to the copyright notice for the Original Work:Licensed under the Academic Free License version 3.0"
-        
-            else if(licences=="Apache license 2.0")
-             licenceInfromation="The Apache 2.0 License is permissive. It allows you to use, modify, and distribute the licensed software, including creating derivative works, without requiring those derivative works to be licensed under the same terms."
-        
-            else if(licences=="MIT")
-             licenceInfromation="The MIT License is a permissive free software license originating at the Massachusetts Institute of Technology (MIT) in the late 1980"
-            
-            else if (licences=="The Unlicense")
-             licenceInfromation="A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code. Permissions."
-*/
-    const readme =
-            `## Project:${answers.title}\n## User Story \n ${answers.discription}\n## Acceptance Criteria \nGIVEN a command-line application that accepts user input
-            \nWHEN I am prompted for information about my application repository
-            \nTHEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-            \nWHEN I enter my project title\n
-            \nTHEN this is displayed as the title of the README
-            \nWHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-            \nTHEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-            \nWHEN I choose a license for my application from a list of options
-            \nTHEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-            \nWHEN I enter my GitHub username
-            \nTHEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-            \nWHEN I enter my email address
-            \nTHEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-            \nWHEN I click on the links in the Table of Contents
-            \nTHEN I am taken to the corresponding section of the README \n ## License:${answers.licences}\n
-            ${answers.licences} is the.\n## Github profile \n
-            https.github\ ${answers.githubName}
-    `;
+
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            const markdownPageContent = generateMarkdown(data);
+
+            fs.writeFile('README.md', markdownPageContent, (err) =>
+            err ? console.log(err) : console.log('Successfully created README.md!')
+            );
+
+        })
+}
 
 
-        fs.writeFile('README.md', readme, (err) =>
-            err ? console.error(err) : console.log('Success!')
-        );
-
-    })
-    .catch((error) => {
-        if (error.isTtyError) {
-            console.log("Prompt couldn't be rendered in the current environment")
-        } else {
-            console.log("Something else went wrong")
-        }
-    });
+init();
